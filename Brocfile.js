ff07e50b145fd8185a686e7f6dfbdb4d4ff624c9
Vsevolod Syrovatskiy <sevavsevolod@gmail.com>
2015-07-03 15:29:46 +0300

    use bower for dropbox.js

--- Brocfile.js
+++ Brocfile.js
@@ -23,7 +23,7 @@ var app = new EmberApp({
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-app.import('vendor/dropbox.js');
-app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+app.import('bower_components/dropbox-build/dropbox.js');
+//app.import('bower_components/bootstrap/dist/js/bootstrap.js');
 
 module.exports = app.toTree();
--- app/routes/gifs.js
+++ app/routes/gifs.js
@@ -1,20 +1,21 @@
 import Ember from 'ember';
+/* global Dropbox */
 
 export default Ember.Route.extend({
   beforeModel: function() {
     var client = new Dropbox.Client({ key: "vvf0ru33x9anjsx" });
 
-      client.authenticate();
+    client.authenticate();
 
     this.set('dropboxClient',client);
   },
   model:function () {
     var client = this.get('dropboxClient');
-    return new Ember.RSVP.Promise(function(resolve,reject) {
+    return new Ember.RSVP.Promise(function (resolve) {
       client.stat('Public/answgif',{readDir:true},function (wtf,data,contents){
         resolve(contents);
       });
-    })
+    });
   },
   setupController:function (c, m) {
     c.set('model',m);

47a4638b993c4e3ed1ccf298dc2822093630e403
SevaMAN <sevavsevolod@gmail.com>
2015-07-03 17:23:43 +0300

    hash router

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'dropgif',
     environment: environment,
     baseURL: '/gifinder/',
-    locationType: 'auto',
+    locationType: 'hash',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
